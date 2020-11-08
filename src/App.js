11c222393a9828b34a85f1cc6cbd97458c47b995
quentintrouve <trouve.quentin@gmail.com>
2020-11-08 18:21:45 +0100

    use state object

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Titre from './Titre';
 
 class App extends Component {
 
+  state = {
+    name1: "Hugo",
+    name2: "Jules",
+    name3: "Eva"
+  }
+
   render() {
     return (
       <div className="App">
-        <Titre name="Hugo">
-          hey
-        </Titre>
-        <Titre name="Jules">
-          hi
-        </Titre>
-        <Titre name="Eva">
-          ha
-        </Titre>
+        <Titre name={this.state.name1} />
+        <Titre name={this.state.name2} />
+        <Titre name={this.state.name3} />
       </div>
     );
   }
