f8339456d69c0ecdf40f88cb7e9cc20bc5dc2483
quentintrouve <trouve.quentin@gmail.com>
2020-11-09 21:50:20 +0100

    clean app folder for start appcontact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import { Component } from 'react';
-import Titre from './Titre/Titre';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
 
-  state = {
-    name1: "Hugo",
-    name2: "Jules",
-    name3: "Eva"
-  }
-
   render() {
     return (
-      <div className="App">
-        <Titre name={this.state.name1} />
-        <Titre name={this.state.name2} />
-        <Titre name={this.state.name3} />
-      </div>
-    );
+
+    )
   }
 }
 
