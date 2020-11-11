226283adad70f8c5e31df596c971f55a63382a48
quentintrouve <trouve.quentin@gmail.com>
2020-11-11 14:11:05 +0100

    feat/ show contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
 
 class Contact extends Component {
+
+  state = {
+    show: true
+  }
+
+  showContact = () => {
+    this.setState({
+      show: !this.state.show,
+    })
+  }
+
   render() {
     return (
-      <div className='card card-body mb-3'>
-        <h4>{this.props.nom}</h4>
-        <ul className='card card-body mb-3'>
-          <li className='list-group-item'>
-            Email : {this.props.email}
-          </li>
-          <li className='list-group-item'>
-            Téléphone : {this.props.tel}
-          </li>
-        </ul>
+      <div className='card card-body mb-3 text-center'>
+        <h4>{this.props.nom}&nbsp; <i className={this.state.show ? 'fas fa-caret-up' : 'fas fa-caret-down'} onClick={this.showContact} style={{cursor: 'pointer'}}></i></h4>
+        {this.state.show ? (
+          <ul className='card card-body mb-3'>
+            <li className='list-group-item'>
+              Email : {this.props.email}
+            </li>
+            <li className='list-group-item'>
+              Téléphone : {this.props.tel}
+            </li>
+          </ul>) : null}
       </div>
     )
   }
